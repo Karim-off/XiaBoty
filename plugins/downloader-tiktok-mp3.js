a8a07342874bc097cd02eaccca1d796df9cf92d3
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-25 21:52:29 -0400

    Actualizar downloader-tiktok-mp3.js

--- plugins/downloader-tiktok-mp3.js
+++ plugins/downloader-tiktok-mp3.js
@@ -1,6 +1,6 @@
 import { capcutdl } from '../lib/capcut.js'
 let handler = async (m, { conn, args, text, usedPrefix, command }) => { 
-  if (!args[0]) throw `ðŸš© *ejemplo:* ${usedPrefix+command} https://vm.tiktok.com/ZMME5XBt3/`;
+  if (!args[0]) throw `*ejemplo:* ${usedPrefix+command} https://vm.tiktok.com/ZMME5XBt3/`;
   let data = await tiktokdl(text);
   await conn.sendMessage(m.chat, { react: { text: "â³", key: m.key } });
 conn.sendMessage(m.chat, { audio: { url: data.originalVideoUrl }, mimetype: 'audio/mpeg', fileName: `${data.title}.mp3` }, { quoted: m })
